faa201f01e72adbd58174be75114dc4bdc6e41f2
Nick Sieger <nick@nicksieger.com>
2012-01-18 09:39:52 -0600

    Generate a bunch of dummy speakers

--- data.rb
+++ data.rb
@@ -8,4 +8,14 @@ SPEAKERS[:jrubyconf] = {
   :twitter => 'jrubyconf'
 }
 
+1.upto(11) do |i|
+SPEAKERS["jrubyconf#{i}".to_sym] = {
+  :name    => 'Coming Soon',
+  :title   => '',
+  :avatar  => 'default.png',
+  :bio     => "<p>Full speaker list and schedule coming soon. Schedule is subject to change.</p>",
+  :twitter => 'jrubyconf'
+}
+end
+
 require 'schedule'
