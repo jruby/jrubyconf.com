60c4e4c0a2e63675bb34f4489816c0a79b427c02
Nick Sieger <nick@nicksieger.com>
2012-01-18 09:36:49 -0600

    Split schedule out into schedule.rb which is to be generated from gcal

---
+++ Rakefile
@@ -0,0 +1,32 @@
+begin
+  require 'bundler/setup'
+rescue
+end
+require 'rake/clean'
+
+CLEAN << 'schedule.rb'
+
+file 'schedule.rb' do |t|
+  require 'erb'
+  erb = ERB.new(File.read('schedule.rb.erb'))
+  File.open(t.name, 'w') {|f| f << erb.result(schedule_binding)}
+end
+
+task :schedule => 'schedule.rb' do
+  # puts schedule_xml
+end
+
+def schedule_xml
+  @doc ||= begin
+             require 'nokogiri'
+             require 'open-uri'
+             require 'yaml'
+             url = YAML.load(File.read('_config.yml'))['schedule_url']
+             open(url) {|f| Nokogiri::XML(f) }
+           end
+end
+
+def schedule_binding
+  binding
+end
+
--- data.rb
+++ data.rb
@@ -1,45 +1,11 @@
 SPEAKERS  = {}
-TUESDAY   = []
-WEDNESDAY = []
 
-SPEAKERS[:adam_mccrea] = {
-  :name    => 'Adam McCrea',
-  :title   => 'Logician at EdgeCase',
-  :avatar  => 'adam_mccreajax.png',
-  :bio     => "<p>Jim Weirich first learned about computers when his college adviser suggested he take a computer science course: \"It will be useful, and you might enjoy it.\" With those prophetic words, Jim has been developing now for over 25 years, working with everything from crunching rocket launch data on supercomputers to wiring up servos and LEDs on micro-controllers.  Currently he loves working in Ruby and Rails as the Chief Scientist at EdgeCase, but you can also find him strumming on his ukulele as time permits.</p>",
-  :twitter => 'adamlogic'
+SPEAKERS[:jrubyconf] = {
+  :name    => 'Coming Soon',
+  :title   => '',
+  :avatar  => 'default.png',
+  :bio     => "<p>Full speaker list and schedule coming soon. Schedule is subject to change.</p>",
+  :twitter => 'jrubyconf'
 }
 
-SPEAKERS[:leon_gersing] = {
-  :name    => 'Leon Gersing',
-  :title   => 'Baller at EdgeCase',
-  :avatar  => 'adam_mccreajax.png',
-  :bio     => "<p>Coming soon</p>"
-}
-
-SPEAKERS[:jerry_nummi] = {
-  :name    => 'Jerry Nummi',
-  :title   => 'Other Duties as Assigned at EdgeCase',
-  :avatar  => 'adam_mccreajax.png',
-  :bio     => "<p>Coming soon</p>"
-}
-
-12.times do
-  TUESDAY << {
-    :time        => '08:00a - 08:45a',
-    :title       => 'Why I love TorqueBox (Why You Will Too)',
-    :description => 'Lorem ipsum',
-    :speaker_id  => 'adam_mccrea',
-    :talk        => true
-  }
-end
-
-12.times do
-  WEDNESDAY << {
-    :time        => '08:00a - 08:45a',
-    :title       => 'Why I love TorqueBox (Why You Will Too)',
-    :description => 'Lorem ipsum',
-    :speaker_id  => 'adam_mccrea',
-    :talk        => true
-  }
-end
+require 'schedule'
