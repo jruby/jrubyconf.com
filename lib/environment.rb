ee2323a016f0e89c6e9fb38d7b9c05c5e3e9672f
Nick Sieger <nick@nicksieger.com>
2012-02-13 15:38:50 -0600

    Add smtp configuration

--- lib/environment.rb
+++ lib/environment.rb
@@ -7,12 +7,6 @@ RACK_ENV = ENV['RACK_ENV'] || "development"
 DB_SETTINGS = YAML.load_file("config/database.yml")[RACK_ENV]
 ActiveRecord::Base.establish_connection(DB_SETTINGS)
 
-# Register .html.erb templates
-require 'tilt'
-Tilt.register Tilt::ERBTemplate, 'html.erb'
-
-require 'mail'
-
 module App
   module Config
     CONFIG_FILE = File.expand_path('../../_config.yml', __FILE__)
@@ -20,6 +14,8 @@ module App
     CONFIG = YAML.load_file(CONFIG_FILE)
     CONFIG.update!(YAML.load_file(LOCAL_CONFIG_FILE)) if File.exist?(LOCAL_CONFIG_FILE)
     SUBMISSION_DEADLINE = CONFIG['submission_deadline']
+
+    module_function
 
     def mail_from
       if production_site?
@@ -36,5 +32,42 @@ module App
     def production_site?
       CONFIG['production_site']
     end
+
+    def smtp_settings
+      @smtp = CONFIG['smtp'] || {}
+    end
+
+    def smtp_host
+      smtp_settings['host']
+    end
+
+    def smtp_domain
+      smtp_settings['domain']
+    end
+
+    def smtp_username
+      smtp_settings['username']
+    end
+
+    def smtp_password
+      smtp_settings['password']
+    end
+  end
+end
+
+# Register .html.erb templates
+require 'tilt'
+Tilt.register Tilt::ERBTemplate, 'html.erb'
+
+require 'mail'
+if App::Config.smtp_host
+  Mail.defaults do
+    delivery_method :smtp, :address => App::Config.smtp_host,
+                           :port => App::Config.smtp_port,
+                           :domain => App::Config.smtp_domain,
+                           :user_name => App::Config.smtp_username,
+                           :password => App::Config.smtp_password,
+                           :authentication => :plain,
+                           :enable_starttls_auto => true
   end
 end
