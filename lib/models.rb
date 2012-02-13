fea5b7575ba55b5069e3648457af631e9655292f
Nick Sieger <nick@nicksieger.com>
2012-02-13 17:56:30 -0600

    Strip @ sign from any twitter handle

--- lib/models.rb
+++ lib/models.rb
@@ -1,11 +1,17 @@
 require 'openssl'
 
 class Proposal < ActiveRecord::Base
+  before_save :check_twitter_at
+
   def key
     self['key'] || generate_key
   end
 
   private
+  def check_twitter_at
+    self['twitter'] = self['twitter'].sub(/^@/,'')
+  end
+
   def generate_key
     OpenSSL::Random.random_bytes(32).unpack('h*')[0]
   end
