0f570ac1199247c7ae58d1d6960cfda12edbd56f
Nick Sieger <nick@nicksieger.com>
2012-02-13 16:56:06 -0600

    Further deploy hook fixing smtp for different domains

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,17 +1,24 @@
-run 'rake deploy_hook'
-raise "rake deploy_hook failed; blog not generated" unless $?.success?
+on_app_servers_and_utilities do
+  run 'rake deploy_hook'
+  raise "rake deploy_hook failed; blog not generated" unless $?.success?
 
-if File.exist?(File.join(shared_path, 'config', 'smtp.yml'))
-  require 'yaml'
-  smtp_config = YAML.load_file(File.join(shared_path, 'config', 'smtp.yml'))
-  domain = node['engineyard']['environment']['apps'].first['vhosts'].first['domain_name'] rescue "_"
-  # Set the production site flag only for the app with the jrubyconf.com domain
-  if domain == 'www.jrubyconf.com'
-    smtp_config = smtp_config.merge "production_site" => true
-  end
-  local_config = File.join(release_path, "_config.local.yml")
-  puts "=+= Writing file: #{local_config}"
-  File.open(local_config, "w") do |f|
-    f << YAML::dump(smtp_config)
+  if File.exist?(File.join(shared_path, 'config', 'smtp.yml'))
+    require 'yaml'
+    smtp_config = YAML.load_file(File.join(shared_path, 'config', 'smtp.yml'))
+
+    domain = node['engineyard']['environment']['apps'].first['vhosts'].first['domain_name'] rescue "_"
+    # Set the production site flag only for the app with the jrubyconf.com domain
+    if domain == 'www.jrubyconf.com'
+      smtp_config = smtp_config.merge "production_site" => true
+    else
+      domain = node['engineyard']['environment']['instances'].first['public_hostname'] rescue "_"
+    end
+    smtp_config['smtp']['domain'] = domain
+
+    local_config = File.join(release_path, "_config.local.yml")
+    puts "=+= Writing file: #{local_config} for domain #{domain}"
+    File.open(local_config, "w") do |f|
+      f << YAML::dump(smtp_config)
+    end
   end
 end
