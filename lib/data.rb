6a08160fe3638b74fb64a604c67cfad62af45f95
Nick Sieger <nick@nicksieger.com>
2012-02-16 09:23:35 -0600

    Add Carin's info, waiting to publish

--- lib/data.rb
+++ lib/data.rb
@@ -18,6 +18,17 @@ SPEAKERS["jrubyconf#{i}".to_sym] = {
 }
 end
 
+CONFIRMED_SPEAKERS = {
+  :carinmeier => {
+    :name => "Carin Meier",
+    :avatar => "carin_meier.jpg",
+    :bio => "Carin is a software developer at EdgeCase. She started off as a professional ballet dancer, studied Physics in college, and has been developing software for both the enterprise and entrepreneur for the past 15 years. She comes from a solid Java background, but has discovered a passion for the simplicity, power, and elegance of Clojure. She brings fun, enthusiasm, and hot tea to all her software projects and especially enjoys participating in the Open Source community.
+
+She lives in Cincinnati, OH with her husband and two young children. When left to daydream, she thinks of the dynamics of flocks of birds, what the Giant Squids might really be doing down there in the deep, and maybe opening a first-rate cheese shop one day.",
+    :twitter => "carinmeier"
+  }
+}
+
 MONDAY    = []
 TUESDAY   = []
 WEDNESDAY = []
