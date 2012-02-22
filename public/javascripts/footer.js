8a433425411f999da7c242b34728ecb483df250f
Nick Sieger <nick@nicksieger.com>
2012-02-21 18:01:46 -0800

    Add footer.js for sharing footer expansion animation code

---
+++ public/javascripts/footer.js
@@ -0,0 +1,7 @@
+// Animate the footer to give us a little more real estate
+$(function() {
+      window.footer = $('#footer');
+      var footerHeight = parseFloat(window.footer.css('height'));
+      footer.mouseenter(function () { window.footer.animate({height: '' + (footerHeight*2) +'px'}, 'fast'); });
+      footer.mouseleave(function () { window.footer.animate({height: '' + footerHeight + 'px'}, 'fast'); });
+  });
