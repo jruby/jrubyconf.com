d2ad4bf376c0257e122dae254523867fad601b43
Nick Sieger <nick@nicksieger.com>
2012-03-12 11:44:20 -0500

    Small tweak to Carin's bio

--- lib/data.rb
+++ lib/data.rb
@@ -43,7 +43,7 @@ SPEAKERS = {
   :carinmeier => {
     :name => "Carin Meier",
     :avatar => "carin_meier.jpg",
-    :bio => "<p>Carin is a software developer at EdgeCase. She started off as a professional ballet dancer, studied Physics in college, and has been developing software for both the enterprise and entrepreneur for the past 15 years. She comes from a solid Java background, but has discovered a passion for the simplicity, power, and elegance of Clojure. She brings fun, enthusiasm, and hot tea to all her software projects and especially enjoys participating in the Open Source community.</p>
+    :bio => "<p>Carin is a software developer at EdgeCase. She started off as a professional ballet dancer, studied Physics in college, and has been developing software for both the enterprise and entrepreneur for the past 15 years. She comes from a solid Java background, but has discovered a passion for the simplicity, power, and elegance of Ruby and Clojure. She brings fun, enthusiasm, and hot tea to all her software projects and especially enjoys participating in the Open Source community.</p>
 
 <p>She lives in Cincinnati, OH with her husband and two young children. When left to daydream, she thinks of the dynamics of flocks of birds, what the Giant Squids might really be doing down there in the deep, and maybe opening a first-rate cheese shop one day.</p>",
     :twitter => "carinmeier"
