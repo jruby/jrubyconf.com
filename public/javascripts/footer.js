a40f545f910f119db26052dd8014bf54c7e9c625
Nick Sieger <nick@nicksieger.com>
2012-05-07 13:09:22 -0500

    Add 8th Light, Jun Group and Getty Images sponsor logos

--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -2,6 +2,6 @@
 $(function() {
       window.footer = $('#footer');
       var footerHeight = parseFloat(window.footer.css('height'));
-      footer.mouseenter(function () { window.footer.animate({height: '' + (footerHeight*2) +'px'}, 'fast'); });
+      footer.mouseenter(function () { window.footer.animate({height: '' + (footerHeight*3) +'px'}, 'fast'); });
       footer.mouseleave(function () { window.footer.animate({height: '' + footerHeight + 'px'}, 'fast'); });
   });
