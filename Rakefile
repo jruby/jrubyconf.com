9e6d321b66236af0ac49b81d9026f4aca5aa98cb
Nick Sieger <nick@nicksieger.com>
2012-05-13 22:01:36 -0500

    Checked in final schedule

--- Rakefile
+++ Rakefile
@@ -5,27 +5,6 @@ end
 
 $:.unshift File.expand_path("../lib", __FILE__)
 
-require 'rake/clean'
-CLEAN << 'lib/schedule.rb' << 'schedule.xml'
-
-require 'schedule_data'
-include JRubyConf::ScheduleData
-
-file 'lib/schedule.rb' do |t|
-  write_schedule_rb(t.name)
-end
-
-file 'schedule.xml' do |t|
-  write_schedule_xml(t.name)
-end
-
-desc "Builds the schedule from GCal"
-task :schedule => ['schedule.xml', 'lib/schedule.rb'] do
-  if Rake.application.options.trace
-    schedule_entries.sort_by {|e| e['Start'] }.each {|e| p e }
-  end
-end
-
 require 'jekyll_regen'
 include JRubyConf::JekyllData
 
---
+++ lib/schedule.rb
@@ -0,0 +1,240 @@
+SUNDAY << {
+  time:        " 9:00am -  4:00pm",
+  title:       "Kids Code Camp",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+SUNDAY << {
+  time:        " 2:00pm -  5:00pm",
+  title:       "RailsBridge Installfest",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+MONDAY << {
+  time:        " 9:00am -  5:00pm",
+  title:       "RailsBridge Workshop",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+MONDAY << {
+  time:        "10:00am - 12:00pm",
+  title:       SPEAKERS[:ajuckel][:talk][:title] || "Getting Started with JRuby",
+  description: SPEAKERS[:ajuckel][:talk][:description] || "",
+  speaker_id:  "ajuckel",
+  talk:        true
+}
+
+MONDAY << {
+  time:        " 1:30pm -  5:00pm",
+  title:       SPEAKERS[:maxdemarzi][:talk][:title] || "Intro to Neo4j",
+  description: SPEAKERS[:maxdemarzi][:talk][:description] || "",
+  speaker_id:  ["maxdemarzi", "ronge"],
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 8:30am -  9:30am",
+  title:       "Breakfast",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 9:15am -  9:30am",
+  title:       "Opening",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 9:30am - 10:30am",
+  title:       SPEAKERS[:venkat_s][:talk][:title] || "It could be heaven or it could be hell: Pleasure and peril of being a polyglot programmer",
+  description: SPEAKERS[:venkat_s][:talk][:description] || "",
+  speaker_id:  "venkat_s",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        "10:45am - 11:30am",
+  title:       SPEAKERS[:headius][:talk][:title] || "JRuby: Full of Surprises",
+  description: SPEAKERS[:headius][:talk][:description] || "",
+  speaker_id:  ["headius", "enebo"],
+  talk:        true
+}
+
+TUESDAY << {
+  time:        "11:30am - 12:00pm",
+  title:       SPEAKERS[:j3][:talk][:title] || "Adventures on the Golden Path",
+  description: SPEAKERS[:j3][:talk][:description] || "",
+  speaker_id:  "j3",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        "12:00pm -  1:30pm",
+  title:       "Lunch",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 1:30pm -  2:00pm",
+  title:       SPEAKERS[:carinmeier][:talk][:title] || "Double Rainbow testing with JRuby and rspec-given",
+  description: SPEAKERS[:carinmeier][:talk][:description] || "",
+  speaker_id:  "carinmeier",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 2:00pm -  2:30pm",
+  title:       SPEAKERS[:codefinger][:talk][:title] || "Deploying JRuby Web Applications",
+  description: SPEAKERS[:codefinger][:talk][:description] || "",
+  speaker_id:  "codefinger",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 3:00pm -  3:30pm",
+  title:       SPEAKERS[:ultrasaurus][:talk][:title] || "Agile Business Development",
+  description: SPEAKERS[:ultrasaurus][:talk][:description] || "",
+  speaker_id:  "ultrasaurus",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 3:30pm -  4:00pm",
+  title:       SPEAKERS[:jremsikjr][:talk][:title] || "Open (Source) Wounds",
+  description: SPEAKERS[:jremsikjr][:talk][:description] || "",
+  speaker_id:  "jremsikjr",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 4:00pm -  5:30pm",
+  title:       "Break out sessions",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 4:15pm -  5:30pm",
+  title:       "Lightning talks",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 5:30pm -  7:00pm",
+  title:       "Dinner on your own",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+TUESDAY << {
+  time:        " 7:00pm - 11:30pm",
+  title:       "Conference Party, Beer and Whisky tasting",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        " 8:30am -  9:30am",
+  title:       "Breakfast",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        " 9:30am - 10:00am",
+  title:       SPEAKERS[:ntschutta][:talk][:title] || "Leading Technical Change",
+  description: SPEAKERS[:ntschutta][:talk][:description] || "",
+  speaker_id:  "ntschutta",
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        "10:00am - 10:30am",
+  title:       SPEAKERS[:yokolet][:talk][:title] || "Pure Java Nokogiri? It&#39;s Just Awesome.",
+  description: SPEAKERS[:yokolet][:talk][:description] || "",
+  speaker_id:  "yokolet",
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        "10:45am - 11:15am",
+  title:       SPEAKERS[:david_wood][:talk][:title] || "JRuby for Performance",
+  description: SPEAKERS[:david_wood][:talk][:description] || "",
+  speaker_id:  "david_wood",
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        "11:15am - 11:45am",
+  title:       SPEAKERS[:xshay][:talk][:title] || "JRuby at Square",
+  description: SPEAKERS[:xshay][:talk][:description] || "",
+  speaker_id:  "xshay",
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        "11:45am - 12:15pm",
+  title:       SPEAKERS[:rthomas][:talk][:title] || "Ruby Y U no GFX?",
+  description: SPEAKERS[:rthomas][:talk][:description] || "",
+  speaker_id:  ["rthomas", "tsaleh"],
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        "12:15pm -  2:15pm",
+  title:       "Lunch",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        " 2:15pm -  2:45pm",
+  title:       SPEAKERS[:ronge][:talk][:title] || "Neo4j.rb and the Revival of a New Type of Object Database",
+  description: SPEAKERS[:ronge][:talk][:description] || "",
+  speaker_id:  "ronge",
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        " 2:45pm -  3:15pm",
+  title:       SPEAKERS[:bascule][:talk][:title] || "Concurrent Programming with Celluloid",
+  description: SPEAKERS[:bascule][:talk][:description] || "",
+  speaker_id:  "bascule",
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        " 3:30pm -  4:30pm",
+  title:       SPEAKERS[:nylons][:talk][:title] || "Dawn of the Devs",
+  description: SPEAKERS[:nylons][:talk][:description] || "",
+  speaker_id:  ["nylons", "irishgirl"],
+  talk:        true
+}
+
+WEDNESDAY << {
+  time:        " 4:20pm -  4:35pm",
+  title:       "Closing",
+  description: "",
+  speaker_id:  "jrubyconf",
+  talk:        true
+}
+
